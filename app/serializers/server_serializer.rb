189ef1d92596303b0c7421892067019ad5db1a5b
behzad <behzadfareed@gmail.com>
2021-01-06 07:01:20 +0330

    sss

--- app/serializers/server_serializer.rb
+++ app/serializers/server_serializer.rb
@@ -1,3 +1,19 @@
 class ServerSerializer < ActiveModel::Serializer
-  attributes :id, :host, :port, :api_key, :server_type, :premium
+  attributes :id, :name, :country, :region, :flag, :host, :port, :api_key, :server_type, :premium
+
+  def region
+    object.country.region
+  end
+
+  def country
+    object.country.name
+  end
+
+  def flag
+    "https://countryflags.io/#{object.country.code}/shiny/64.png"
+  end
+
+  def name
+    object.country.name
+  end
 end
