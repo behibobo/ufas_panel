8edafd4ae36768ee2cd99fb541f31938730f05fc
behzad <behzadfareed@gmail.com>
2021-01-09 10:32:41 +0330

    sss

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+uri = URI.parse("https://restcountries.eu/rest/v2/all")
+
+        response = Net::HTTP.get_response(uri)
+        hash = JSON.parse(response.body)
+        Country.destroy_all
+        hash.each do |c|
+            Country.create(name: c["name"], region: c["region"], code: c["alpha2Code"])
+        end
+        
 Plan.create(name: "gift", days:7, price: 0)
 10.times do
     Server.create!(
